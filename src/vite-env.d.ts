cc77393480b8055aaba927a5f85cd05e9929b582
xin-sui <2505258205@qq.com>
2023-12-25 17:32:05 +0800

    完成-展映时段

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,3 +3,10 @@ declare module 'parallax-js' {
     const Parallax: any;
     export default Parallax;
 }
+declare module "*.vue" {
+    import type { DefineComponent } from "vue";
+
+    const vueComponent: DefineComponent<{}, {}, any>;
+
+    export default vueComponent;
+}
